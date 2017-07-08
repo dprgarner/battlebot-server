7dc9fd392e1da33b9c876af29fb061b76f9070c3
David Garner <dprgarner@gmail.com>
2017-07-08 21:37:44 +0100

    Add a note about the GraphQL API.

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const Promise = require('bluebird')
 const MongoClient = Promise.promisifyAll(require('mongodb').MongoClient);
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/battlebots';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/battlebots2';
 
 function connect(promiseGenerator) {
   // Handle database connection and disconnection, throwing any errors after
