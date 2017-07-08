9810e3650e1d5685249ea3d4172cbe40a6265a08
David Garner <dprgarner@gmail.com>
2017-07-08 20:39:29 +0100

    Split out contest resolvers

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,6 @@
 const _ = require('underscore');
 const { gameTypes } = require('./typeDefs');
+const contestResolvers = require('./contestResolvers');
 
 const baseGameResolver = {
   id: ({ _id }) => _id,
@@ -11,48 +12,6 @@ const baseGameResolver = {
     victor && Bot.load({ game, bot_id: victor })
   ),
 };
-
-function sortScore(botsToSort) {
-  botsToSort.sort((bot1, bot2) => {
-    if (bot1.score > bot2.score) return -1;
-    if (bot1.score < bot2.score) return 1;
-
-    if (bot1.wins > bot2.wins) return -1;
-    if (bot1.wins < bot2.wins) return 1;
-
-    if (bot1.losses > bot2.losses) return 1;
-    if (bot1.losses < bot2.losses) return -1;
-
-    return 0;
-  });
-}
-
-function rewardWins(bots) {
-  let sortedBots = _.map(bots, bot => _.extend(
-    { score: 3 * bot.wins + bot.draws }, bot
-  ));
-
-  sortScore(sortedBots);
-  return sortedBots;
-}
-
-function punishLosses(bots) {
-  let sortedBots = _.map(bots, bot => _.extend(
-    { score: 3 * bot.wins + 2 * bot.draws }, bot
-  ));
-
-  sortScore(sortedBots);
-  return sortedBots;
-}
-
-function balanced(bots) {
-  let sortedBots = _.map(bots, bot => _.extend(
-    { score: 2 * bot.wins + bot.draws }, bot
-  ));
-
-  sortScore(sortedBots);
-  return sortedBots;
-}
 
 const resolvers = {
   Query: {
@@ -83,52 +42,6 @@ const resolvers = {
     },
   },
 
-  Contest: {
-    name: ({ contest }) => contest,
-    gameType: ({ game }) => game,
-    games: ({ contest, game }, { filters }, { Games }) => (
-      Games.load(_.extend({ contest, game }, _.omit(filters, 'contest')))
-    ),
-    rankings: ({ contest, game }, { method }, { Games }) => (
-      Games.load({ contest, game }).then(gameDocuments => {
-        const bots = {};
-
-        gameDocuments.forEach(game => {
-          game.players.forEach(player => {
-            if (!bots[player]) bots[player] = {
-              game: game.game,
-              bot_id: player,
-              wins: 0,
-              losses: 0,
-              draws: 0,
-            };
-            if (game.victor === player) {
-              bots[player].wins += 1;
-            } else if (!game.victor) {
-              bots[player].draws += 1;
-            } else {
-              bots[player].losses += 1;
-            }
-          });
-        });
-
-        switch (method) {
-          case 'punitive':
-            return punishLosses(bots);
-          case 'balanced':
-            return balanced(bots);
-          case 'ambitious':
-          default:
-            return rewardWins(bots);
-        };
-      })
-    ),
-  },
-
-  ContestRanking: {
-    bot: ({ bot_id, game }, _, { Bot }) => Bot.load({ game, bot_id }),
-  },
-
   NoughtsAndCrosses: baseGameResolver,
 
   NoughtsAndCrossesMarks: {
@@ -146,5 +59,7 @@ const resolvers = {
     ),
   },
 };
+
+_.extend(resolvers, contestResolvers);
 
 module.exports = resolvers;
