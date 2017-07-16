3d57f7a89dfb87d7a7167d28288ee9dd78006c32
David Garner <dprgarner@gmail.com>
2017-07-16 13:29:59 +0100

    Move socket drivers into sockets file

--- src/ws/cycle.js
+++ src/ws/cycle.js
@@ -1,89 +1,7 @@
-/*
-  Experiments in running the websocket server with CycleJS.
-*/
-import Rx from 'rxjs/Rx';
-import WebSocket from 'ws';
-import { adapt } from '@cycle/run/lib/adapt';
+import Rx from 'rxjs';
 import { run } from '@cycle/rxjs-run';
 
-import { createRandomHash } from '../hash';
-import { wsObserver, wsObservable } from './sockets';
-
-export const OPEN = 'open';
-export const INCOMING = 'incoming';
-export const OUTGOING = 'outgoing';
-export const CLOSE = 'close';
-export const ERROR = 'error';
-
-function createWsUpdate$(socketId, ws, outgoing$) {
-  // Handles websocket-specific listeners, tagging and untagging instructions
-  // to send and receive messages to the client and closing the connection.
-  outgoing$
-    .takeUntil(outgoing$.filter(
-      ({ type }) => type === CLOSE || type === ERROR
-    ))
-    .filter(({ type }) => type === OUTGOING)
-    .map(({ payload }) => payload)
-    .subscribe(wsObserver(ws));
-
-  const incoming$ = wsObservable(ws);
-  incoming$.subscribe({ error: () => ws.close(1002, 'ERROR') });
-
-  return incoming$
-    .map(payload => ({ type: INCOMING, socketId, payload }))
-    .startWith({ type: OPEN, socketId })
-    .concat(Rx.Observable.of({ type: CLOSE, socketId }))
-    .catch(error => Rx.Observable.of(
-      { type: ERROR, socketId, payload: { error }}
-    ));
-}
-
-function getOpenSockets$(update$) {
-  // Given the websocket update stream, this function returns a stream which
-  // emits streams every time a socket connects or disconnects. These emitted
-  // streams emit every currently-open socket. (I guess it could just be an
-  // emitted array, but hey, streams)
-  return update$
-    .filter(({ type }) => type !== INCOMING)
-    .startWith({})
-    .scan((sockets, { type, socketId }) => {
-      if (type === OPEN) sockets[socketId] = true;
-      if (type === CLOSE || type === ERROR) delete sockets[socketId];
-      return sockets;
-    });
-}
-
-function makeWsDriver(opts) {
-  // A Cycle.js driver for a WebSocket server.
-  console.log(`Starting WebSocket server...`);
-  const wss = new WebSocket.Server(opts);
-
-  function WsDriver(outgoing$) {
-    // Convert CycleJS xstream into an RxJS stream.
-    outgoing$ = Rx.Observable.from(outgoing$);
-
-    const ws$ = Rx.Observable.create(observer => {
-      wss.on('connection', (ws) => {
-        const socketId = createRandomHash();
-        const wsOutgoing$ = outgoing$
-          .filter(({ socketId: id }) => socketId === id);
-
-        const update$ = createWsUpdate$(socketId, ws, wsOutgoing$);
-        observer.next(update$);
-      });
-    }).share();
-
-    const incoming$ = adapt(ws$.mergeAll());
-
-    // For convenience: a stream which emits an array of all open sockets
-    // whenever a socket opens or closes.
-    incoming$.sockets$ = getOpenSockets$(incoming$);
-    return incoming$;
-  }
-
-  return WsDriver;
-}
-
+import makeWsDriver, { CLOSE, OUTGOING }  from './sockets';
 
 function main(sources) {
   const inc$ = sources.ws;
@@ -91,7 +9,7 @@ function main(sources) {
   const timer = Rx.Observable.interval(1000);
 
   const out$ = timer.withLatestFrom(sockets$, (time, sockets) => {
-    const socket$ = Rx.Observable.from(Object.keys(sockets));
+    const socket$ = Rx.Observable.from(sockets);
 
     if (time % 10 === 0) {
       return socket$.map(socketId => ({ type: CLOSE, socketId }));
--- src/ws/sockets.js
+++ src/ws/sockets.js
@@ -1,5 +1,17 @@
+/*
+  Experiments in running the websocket server with CycleJS.
+*/
 import Rx from 'rxjs';
 import WebSocket from 'ws';
+import { adapt } from '@cycle/run/lib/adapt';
+
+import { createRandomHash } from '../hash';
+
+export const OPEN = 'open';
+export const INCOMING = 'incoming';
+export const OUTGOING = 'outgoing';
+export const CLOSE = 'close';
+export const ERROR = 'error';
 
 export function wsObserver(ws) {
   // An observer for outgoing JSON messages to the socket.
@@ -33,6 +45,44 @@ export function wsObservable(ws) {
   }).share();
 }
 
+function createWsUpdate$(socketId, ws, outgoing$) {
+  // Handles websocket-specific listeners, tagging and untagging instructions
+  // to send and receive messages to the client and closing the connection.
+  outgoing$
+    .takeUntil(outgoing$.filter(
+      ({ type }) => type === CLOSE || type === ERROR
+    ))
+    .filter(({ type }) => type === OUTGOING)
+    .map(({ payload }) => payload)
+    .subscribe(wsObserver(ws));
+
+  const incoming$ = wsObservable(ws);
+  incoming$.subscribe({ error: () => ws.close(1002, 'ERROR') });
+
+  return incoming$
+    .map(payload => ({ type: INCOMING, socketId, payload }))
+    .startWith({ type: OPEN, socketId })
+    .concat(Rx.Observable.of({ type: CLOSE, socketId }))
+    .catch(error => Rx.Observable.of(
+      { type: ERROR, socketId, payload: { error }}
+    ));
+}
+
+function getOpenSockets$(update$) {
+  // Given the websocket update stream, this function returns a stream which
+  // emits an array of open socket IDs every time a socket connects or
+  // disconnects.
+  return update$
+    .filter(({ type }) => type !== INCOMING)
+    .startWith({})
+    .scan((sockets, { type, socketId }) => {
+      if (type === OPEN) sockets[socketId] = true;
+      if (type === CLOSE || type === ERROR) delete sockets[socketId];
+      return sockets;
+    })
+    .map(sockets => Object.keys(sockets));
+}
+
 export function createWebsocketStream(opts) {
   // A stream of WebSockets.
   return Rx.Observable.create(observer => {
@@ -48,4 +98,34 @@ export function createWebsocketStream(opts) {
       console.log('Server Closed');
     });
   }).share();
+}
+
+export default function makeWsDriver(opts) {
+  // A Cycle.js driver for a WebSocket server.
+  const ws$ = createWebsocketStream(opts);
+
+  function WsDriver(outgoing$) {
+    // Convert CycleJS xstream into an RxJS stream.
+    outgoing$ = Rx.Observable.from(outgoing$);
+
+    // Must have .share as the flatMap contains some unabashed side-effects.
+    const incoming$ = adapt(
+      ws$.flatMap(ws => {
+        const socketId = createRandomHash();
+        const wsOutgoing$ = outgoing$
+          .filter(({ socketId: id }) => socketId === id);
+
+        const update$ = createWsUpdate$(socketId, ws, wsOutgoing$);
+        return update$;
+      })
+      .share()
+    );
+    // For convenience: a stream which emits an array of all open sockets
+    // whenever a socket opens or closes.
+    incoming$.sockets$ = getOpenSockets$(incoming$);
+
+    return incoming$;
+  }
+
+  return WsDriver;
 }
