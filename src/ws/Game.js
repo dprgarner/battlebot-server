9270bd78f1b9a06b25588bfe68caba96f0b7901f
David Garner <dprgarner@gmail.com>
2017-07-20 20:08:24 +0100

    quitting while it's not broken :(

--- src/ws/Game.js
+++ src/ws/Game.js
@@ -1,10 +1,60 @@
+import _ from 'underscore';
 import Rx from 'rxjs';
+
+// import getVictor from './getVictor';
+import games from '../games';
 import makeWsDriver, { CLOSE, INCOMING, OUTGOING }  from './sockets';
+import { createShortRandomHash } from '../hash';
+
+function runGame(validator, reducer) {
+  // This function transforms a stream of incoming turns into a stream of updates,
+  // validating the turns and updating the state of the game.
+
+  // The 'reducer' can in general be non-deterministic (make random calls),
+  // so every scan must be evaluated EXACTLY once, regardless of the
+  // subscriptions. (Hence the shareReplay.)
+  return ({ incoming }) => {
+    const update$ = incoming.scan(({ state: oldState }, turn) => {
+      let valid = false;
+      let state = oldState;
+      let log = null;
+      try {
+        valid = validator(oldState, turn);
+      } catch (e) {
+        log = e.message;
+      }
+      if (valid) {
+        state = reducer(oldState, turn);
+      }
+      turn.valid = valid;
+      return { state, turn, log };
+    })
+    .shareReplay();
+
+    return {
+      update: turn$.map(({ state, turn }) => ({ state, turn })),
+      log: turn$.map(({ log }) => log).filter(msg => msg),
+    };
+  };
+}
+
+function addLastTurn(update$) {
+  return update$
+    .takeWhile(({ state: { complete } }) => !complete)
+    .concat(update$
+      .skipWhile(({ state: { complete } }) => !complete)
+      .take(1)
+    );
+}
 
 export default function Game(sources) {
-  const props$ = sources.props;
+  const props$ = sources.props.first();
+
 
   const tick$ = Rx.Observable.interval(1000);
+
+
+
 
   return {
     complete: props$.switchMap(props =>
