91812e5df283f7eea6cb889b867c549a8fd9743f
David Garner <dprgarner@gmail.com>
2017-08-06 22:03:42 +0100

    Fix ClientError bug

--- src/http/graphql/mutations.js
+++ src/http/graphql/mutations.js
@@ -1,4 +1,4 @@
-import ClientError from '../error';
+import { ClientError } from '../error';
 import connect from '../../db';
 import games from '../../games';
 import { createRandomHash } from '../../hash';
