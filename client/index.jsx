249da95516f897ed54a174071dc4640639f15592
David Garner <dprgarner@gmail.com>
2017-09-07 23:54:32 +0100

    Add a daft styled component

--- client/index.jsx
+++ client/index.jsx
@@ -1,15 +1,34 @@
+import styled from 'styled-components';
+
 import React from 'react';
 import { render } from 'react-dom';
 
 import 'normalize.css';
 
-window.onload = () => {
-  render(
-    <div>
-      <a href="https://github.com/dprgarner/battlebot-server">
-      { 'Github page for the Battlebot Server' }
-      </a>
-    </div>,
-    document.getElementById('app')
+const Body = styled.div`
+  height: calc(100vh - 100px);
+  padding: 50px;
+`;
+
+const BigContainer = styled.article`
+  background-color: #e0e0e0;
+  border-radius: 15px;
+  border: 1px solid #888;
+  padding: 30px;
+`;
+
+function App() {
+  return (
+    <Body>
+      <BigContainer>
+        <a href="https://github.com/dprgarner/battlebot-server">
+          { 'Github page for the Battlebot Server' }
+        </a>
+      </BigContainer>
+    </Body>
   );
+}
+
+window.onload = () => {
+  render(<App />, document.getElementById('app'));
 };
